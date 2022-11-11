a33ef3306c8d68e5bb5cc36ca17ad901f11dcb79
Roberto Brown <robert524132@gmail.com>
2022-11-11 09:28:19 -0500

    Update config.php
    config user and pass for azure

--- class/config.php
+++ class/config.php
@@ -2,7 +2,7 @@
 /*Roberto Brown
 8-893-2450*/
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','azure');
+define('DB_PASS','6#vWHD_$');
 define('DB_NAME','dblibros');
 ?>
