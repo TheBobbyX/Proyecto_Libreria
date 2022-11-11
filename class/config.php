e0b1a52c6257183a8531477cc3a51a793db3a6aa
Roberto Brown <robert524132@gmail.com>
2022-11-11 09:47:44 -0500

    Update config.php
    config host and port database

--- class/config.php
+++ class/config.php
@@ -1,7 +1,7 @@
 <?php
 /*Roberto Brown
 8-893-2450*/
-define('DB_HOST','localhost');
+define('DB_HOST','127.0.0.1:51584');
 define('DB_USER','azure');
 define('DB_PASS','6#vWHD_$');
 define('DB_NAME','dblibros');
